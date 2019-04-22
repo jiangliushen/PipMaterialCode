f7b7e8fb8b936a1e85af2f1870c749f3fa30f7bd
jiangliushen <775239404@qq.com>
2019-04-22 11:00:48 +0800

    校验文件
    检验管子管件和螺栓垫片的表格

--- PipMaterialCode/Form1.Designer.cs
+++ PipMaterialCode/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace PipMaterialCode
 {
-    partial class Form1
+    partial class PipMaterialCode
     {
         /// <summary>
         /// 必需的设计器变量。
@@ -28,13 +28,81 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(PipMaterialCode));
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.tbApply = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(536, 44);
+            this.btnCheck.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(80, 29);
+            this.btnCheck.TabIndex = 0;
+            this.btnCheck.Text = "校验文件";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // tbApply
+            // 
+            this.tbApply.Location = new System.Drawing.Point(114, 45);
+            this.tbApply.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.tbApply.Name = "tbApply";
+            this.tbApply.Size = new System.Drawing.Size(404, 26);
+            this.tbApply.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.label1.Location = new System.Drawing.Point(41, 48);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "申请表：";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(626, 44);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "打开文件";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // PipMaterialCode
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.ClientSize = new System.Drawing.Size(713, 164);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbApply);
+            this.Controls.Add(this.btnCheck);
+            this.Font = new System.Drawing.Font("微软雅黑", 10.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "PipMaterialCode";
+            this.Text = "PipMaterialCode-SHEN";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnCheck;
+        private System.Windows.Forms.TextBox tbApply;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- PipMaterialCode/Program.cs
+++ PipMaterialCode/Program.cs
@@ -16,7 +16,7 @@ namespace PipMaterialCode
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new PipMaterialCode());
         }
     }
 }
